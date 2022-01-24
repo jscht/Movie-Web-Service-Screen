24eae679c297f4d4ce76479528bcac3de738416a
jscht <80654820+jscht@users.noreply.github.com>
2022-01-24 12:54:01 +0900

    Add files via upload

---
+++ react-cra/src/App.js
@@ -0,0 +1,12 @@
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to React!</h1>
+      <Button text="Tour of CRA 02:30" />
+    </div>
+  );
+}
+
+export default App;
