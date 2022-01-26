0c03a2a319a523428bd0aa78fa08db17d424f9a6
jscht <sork6260@naver.com>
2022-01-26 18:48:16 +0900

    commit from vsc

--- practice-movie-app/src/App.js
+++ practice-movie-app/src/App.js
@@ -1,12 +1,34 @@
 import { useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const onChange = (event) => setToDo(event.target.value);
-  console.log(toDo)
+  const [toDoVal, setToDoVal] = useState("");
+  const [toDos, setToDos] = useState([]);
+  console.log(toDos);
+  const onChange = (event) => setToDoVal(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDoVal === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDoVal, ...currentArray]);
+    // 배열 원소 위치를 바꾸면 원하는 위치에 저장시킬 수 있다.
+    setToDoVal("");
+    console.log(toDos);
+    // 여기서는 바로 추가되지 않는다
+  };
+  
   return (
     <div className="App">
-      <input onChange={onChange} type="text" placeholder="Write your to do..." />
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          onChange={onChange} 
+          value={toDoVal} 
+          type="text" 
+          placeholder="Write your to do..." 
+        />
+        <button onClick={onSubmit}>Add To Do</button>
+      </form>
     </div>
   );
 }
