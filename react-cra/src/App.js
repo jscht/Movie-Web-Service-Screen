455bcea22d716e6233220b657de9e023ba6110a5
jscht <sork6260@naver.com>
2022-01-26 17:07:10 +0900

    commit from vsc

--- react-cra/src/App.js
+++ react-cra/src/App.js
@@ -1,54 +1,30 @@
 import Button from './Button';
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
+function Hello() {
+  // 주로 react를 사용할 떄는 따로 function을 작성하지않고 useEffect에 작성
+  // useEffect(() => {
+  //   console.log("hi");
+  //   return () => console.log("bye");
+  // }, []);
+
+  useEffect(function() {
+    console.log("hi");
+    return function() {
+      console.log("bye");
+    };
+  }, []);
+    
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [value, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onChange = (event) => {
-    setKeyword(event.target.value);
-  }
-  const prevValue = () => {
-    setValue((prev) => prev + 1)
-    console.log("clicked")
-  };
-
-  useEffect(() => {
-    console.log("I run only once.")
-  }, []); 
-  // react.js가 아무것도(빈 배열이기 떄문) 보고 있지 않으니 한번만 실행된다.
-
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.")
-  }, [keyword]);
-  // keyword가 변화하는 것을 react.js가 보고 해당 코드를 실행시킨다.
-
-  useEffect(() => {
-    console.log("I run when 'value' changes.")
-  }, [value]);
-  
-  useEffect(() => {
-    console.log("I run when 'keyword & value' changes.")
-  }, [keyword, value]);
-  // 둘 중에 하나라도 변화가 일어나면 이 코드가 실행된다.
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => {setShowing((prev) => !prev)}
   return (
     <div className="App">
-      <div>
-        <h1 className={styles.title}>Welcome to React!</h1>
-        <Button text="Tour of CRA" />
-      </div>
-
-      <div>
-        <h3>{value}</h3>
-        <input 
-          value={keyword} 
-          onChange={onChange} 
-          text="text" 
-          placeholder='Search here...' 
-        />
-        <Button click={prevValue} text="click me!" />
-      </div>
+      {showing ? <Hello /> : null}
+      <Button showing={showing} click={onClick} text='Show'/>
     </div>
   );
 }
--- react-cra/src/Button.js
+++ react-cra/src/Button.js
@@ -2,14 +2,19 @@ import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 
 function Button(props){
+    //console.log(props)
     return(
         <button 
             className={styles.btn}
             onClick={props.click}
         >
-            {props.text}
+            {props.showing ? "Hide" : props.text}
         </button>
     )
+}
+
+Button.defaultProps = { // default 값을 주는 방법
+    // text: 'xx'
 }
 
 Button.propTypes = {
