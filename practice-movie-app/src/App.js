2b4554d1a97bcd0fa7a8c578dcc332027d126a89
jscht <sork6260@naver.com>
2022-02-04 02:53:44 +0900

    02040253

--- practice-movie-app/src/App.js
+++ practice-movie-app/src/App.js
@@ -3,24 +3,31 @@ import { useEffect, useState } from "react";
 function App() {
   // 로딩을 위한 state
   const [loading, setLoading] = useState(true);
+  // 코인 임시 리스트 state
+  const [coins, setCoins] = useState([]);
+
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
     .then(response => 
       response.json()
     ).then((json) => {
-      console.log(json)
       setCoins(json);
       setLoading((curr) => !curr);
     })
   }, []);
-  
-  // 코인 임시 리스트 state
-  const [coins, setCoins] = useState([]);
 
   // 사용자 입력 달러 state
-  const [dollars, setDollars] = useState('');
-  const inputDollars = (event) => {setDollars(event.target.value)}
+  const [dollars, setDollars] = useState(1);
+  const inputDollars = (event) => {
+    setDollars(event.target.value)
+  }
 
+  // 코인 변환, 단위 state
+  const [cng, setCng] = useState(1);
+  const onCng = (event) => {
+    setCng(event.target.value)
+  }
+  
   return (
     <div className="App">
 
@@ -30,16 +37,19 @@ function App() {
         :(
           <div>
             <h2>coin tracker</h2>
-            <input onChange={inputDollars} value={dollars} type="number" placeholder="input your dollar you have..." />
-            <h3>{dollars}</h3>
-            <select>
+            <label htmlFor="dollars" >Your dollars
+            <input id="dollars" onChange={inputDollars} value={dollars} type="number" placeholder="input your dollar you have..." />
+              USD</label>
+            <br/>
+            <select onChange={onCng}>
               {coins.map((coin, index) =>
               // 이 coin은 coins array안에 있는 각각의 coin을 의미한다.
-                <option key={index}>
-                  {coin.name}({coin.symbol}): $ {coin.quotes.USD.price} USD
+                <option key={index} value={coin.quotes.USD.price}>
+                  {coin.name}({coin.symbol})
                 </option>
               )}
             </select>
+            <h3>{dollars/cng}</h3>
           </div>
         )}
     </div>
