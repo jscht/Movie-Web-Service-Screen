87c23d147d36b36e8db7003c7550614cd1d207e4
jscht <sork6260@naver.com>
2022-02-03 17:48:13 +0900

    commit from vsc 02031748

--- practice-movie-app/src/App.js
+++ practice-movie-app/src/App.js
@@ -1,46 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDoVal, setToDoVal] = useState("");
-  const [toDos, setToDos] = useState([]);
-  console.log(toDos);
-  const onChange = (event) => setToDoVal(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDoVal === "") {
-      return;
-    }
-    //setToDos((currentArray) => [toDoVal, ...currentArray]);
-    setToDos((currentArray) => [...currentArray, toDoVal]);
-    // 함수를 보낼 때 react.js는 함수의 첫 번째 argument로 현재 state를 보낸다.
-    // 위의 코드는 현재 toDos배열을 가져와서 새로운 toDos의 array로 return하고 있다.
-    setToDoVal("");
-  };
-  const delToDo = (index) => {
-    //console.log(index)
-    setToDos(toDos.filter((item, toDoIndex) => index !== toDoIndex))
-  };
+  // 로딩을 위한 state
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => 
+      response.json()
+    ).then((json) => {
+      console.log(json)
+      setCoins(json);
+      setLoading((curr) => !curr);
+    })
+  }, []);
+  
+  // 코인 임시 리스트 state
+  const [coins, setCoins] = useState([]);
+
+  // 사용자 입력 달러 state
+  const [dollars, setDollars] = useState('');
+  const inputDollars = (event) => {setDollars(event.target.value)}
+
   return (
     <div className="App">
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input 
-          onChange={onChange} 
-          value={toDoVal} 
-          type="text" 
-          placeholder="Write your to do..." 
-        />
-        <button onClick={onSubmit}>Add To Do</button>
-      </form>
-      <hr/>
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>
-            {item}&emsp;
-            <button onClick={() => delToDo(index)}>❌</button>
-          </li>
-        ))}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {
+        loading ? <strong>Loading...</strong>
+        :(
+          <div>
+            <h2>coin tracker</h2>
+            <input onChange={inputDollars} value={dollars} type="number" placeholder="input your dollar you have..." />
+            <h3>{dollars}</h3>
+            <select>
+              {coins.map((coin, index) =>
+              // 이 coin은 coins array안에 있는 각각의 coin을 의미한다.
+                <option key={index}>
+                  {coin.name}({coin.symbol}): $ {coin.quotes.USD.price} USD
+                </option>
+              )}
+            </select>
+          </div>
+        )}
     </div>
   );
 }
